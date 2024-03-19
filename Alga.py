b420d4f8381585827fe9063d5555e0d62d4dee69
aldia2000 <163038287+aldia2000@users.noreply.github.com>
2024-03-19 16:07:58 -0400

    Update Alga.py

--- Alga.py
+++ Alga.py
@@ -1,11 +1,11 @@
 import math
 
-# Définition d'une fonction pour calculer la distance entre deux points géographiques
+# On défini une fonction pour calculer la distance entre deux points géographiques
 def distance(lat1, lon1, lat2, lon2):
     # Rayon moyen de la Terre en kilomètres
     R = 6371.0
     
-    # Conversion des coordonnées degrés en radians
+    # On converti des coordonnées degrés en radians
     lat1 = math.radians(lat1)
     lon1 = math.radians(lon1)
     lat2 = math.radians(lat2)
@@ -16,6 +16,7 @@ def distance(lat1, lon1, lat2, lon2):
     dlat = lat2 - lat1
     
     # Formule de la distance haversine
+    #Cette formule permet de calculer la distance entre deux points sur la surface de la Terre en prenant en compte la courbure de la Terre
     a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     distance = R * c
@@ -24,11 +25,8 @@ def distance(lat1, lon1, lat2, lon2):
 
 # Fonction pour obtenir les coordonnées et le nom d'un point
 def get_point_info():
-    # Demander à l'utilisateur de saisir le nom du point
     name = input("Entrez le nom du point : ")
-    # Demander à l'utilisateur de saisir la latitude du point
     lat = float(input("Latitude du point {} : ".format(name)))
-    # Demander à l'utilisateur de saisir la longitude du point
     lon = float(input("Longitude du point {} : ".format(name)))
     return name, lat, lon
 
